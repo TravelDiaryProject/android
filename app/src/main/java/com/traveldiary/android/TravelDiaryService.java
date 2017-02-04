9f0f69a49265ea7b4a8ff516bd37f371d8c636e6
Cyborg <s.o.lavrynenko@gmail.com>
2017-02-04 20:23:00 +0200

    added creating trip

--- app/src/main/java/com/traveldiary/android/TravelDiaryService.java
+++ app/src/main/java/com/traveldiary/android/TravelDiaryService.java
@@ -6,6 +6,8 @@ import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
@@ -23,12 +25,12 @@ public interface TravelDiaryService {
     @GET("/api/v1/trip/{id}/places")
     Call<List<Place>> listPlacesByTrip(@Path("id") int groupId);
 
-    /*@Multipart
-    @POST("/api/v1/place")
-    Call<ResponseBody> postImage(@Part MultipartBody.Part image);*/
-
     @Multipart
     @POST("/api/v1/place")
-    Call<ResponseBody> postImage(@Part("photo") RequestBody photo);
+    Call<ResponseBody> postImage(@Part MultipartBody.Part image, @Part("tripId") RequestBody tripId);
+
+    @FormUrlEncoded
+    @POST("/api/v1/trip")
+    Call<ResponseBody> createTrip(@Field("title") String tripTitle);
 
 }
